1c5f6ae5f62c058059ea3c98221750b08b637ab4
marcus8448 <28928887+marcus8448@users.noreply.github.com>
2021-08-16 17:17:15 -0700

    feat: struct test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello World!");
+    let box_ = Box {
+        width: 10,
+        height: 20,
+        depth: 4
+    };
+
+    assert_eq!(volume(&box_), 10 * 20 * 4);
+    assert_eq!(volume(&box_), 10 * 20 * 4);
+    assert_eq!(box_.volume(), 10 * 20 * 4);
+    assert_eq!(box_.volume(), 10 * 20 * 4);
+
+}
+
+struct Box {
+    width: u32,
+    height: u32,
+    depth: u32
+}
+
+impl Box {
+    fn volume (&self) -> u32 {
+        return self.width * self.height * self.depth;
+    }
+}
+
+fn volume(box_: &Box) -> u32 {
+    return box_.width * box_.depth * box_.height;
 }
